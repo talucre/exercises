12b7225ecb18846ecc96ad31aa59bb9c435ab767
talucre <talucreee@gmail.com>
2025-10-25 15:27:35 +0300

    exercises 1.1-1.2

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,20 @@
+import Part from "./Part";
+
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
+  );
+};
+
+export default Content;
