7ef51b39c8966029b627c4713c2720908b75e209
talucre <talucreee@gmail.com>
2026-01-05 00:08:40 +0300

    part 7c

--- part7/routed-notes/src/main.jsx
+++ part7/routed-notes/src/main.jsx
@@ -1,17 +1,57 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     Link,
     Navigate,
-    useParams,
     useNavigate,
     useMatch,
 } from 'react-router-dom'
+import {
+    Container,
+    TableContainer,
+    Table,
+    Paper,
+    TableBody,
+    TableCell,
+    TableRow,
+    TextField,
+    Alert,
+    AppBar,
+    Toolbar,
+} from '@mui/material'
+import styled from 'styled-components'
 
+const Button = styled.button`
+    background: Bisque;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid Chocolate;
+    border-radius: 3px;
+`
+
+const Input = styled.input`
+    margin: 0.25em;
+`
+
+const Page = styled.div`
+    padding: 1em;
+    background: papayawhip;
+`
+
+const Navigation = styled.div`
+    background: BurlyWood;
+    padding: 1em;
+`
+
+const Footer = styled.div`
+    background: Chocolate;
+    padding: 1em;
+    margin-top: 1em;
+`
 const Home = () => (
     <div>
         <h2>TKTL notes app</h2>
@@ -44,13 +84,22 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
     <div>
         <h2>Notes</h2>
-        <ul>
-            {notes.map(note => (
-                <li key={note.id}>
-                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
-                </li>
-            ))}
-        </ul>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableBody>
+                    {notes.map(note => (
+                        <TableRow key={note.id}>
+                            <TableCell>
+                                <Link to={`/notes/${note.id}`}>
+                                    {note.content}
+                                </Link>
+                            </TableCell>
+                            <TableCell>{note.user}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     </div>
 )
 
@@ -79,12 +128,16 @@ const Login = props => {
             <h2>login</h2>
             <form onSubmit={onSubmit}>
                 <div>
-                    username: <input />
+                    <Input />
                 </div>
                 <div>
-                    password: <input type="password" />
+                    <Input type="password" />
                 </div>
-                <button type="submit">login</button>
+                <div>
+                    <Button type="submit" primary="">
+                        login
+                    </Button>
+                </div>
             </form>
         </div>
     )
@@ -113,9 +166,14 @@ const App = () => {
     ])
 
     const [user, setUser] = useState(null)
+    const [message, setMessage] = useState(null)
 
     const login = user => {
         setUser(user)
+        setMessage(`welcome ${user}`)
+        setTimeout(() => {
+            setMessage(null)
+        }, 10000)
     }
 
     const padding = {
@@ -128,8 +186,8 @@ const App = () => {
         : null
 
     return (
-        <div>
-            <div>
+        <Page>
+            <Navigation>
                 <Link style={padding} to="/">
                     home
                 </Link>
@@ -146,7 +204,7 @@ const App = () => {
                         login
                     </Link>
                 )}
-            </div>
+            </Navigation>
 
             <Routes>
                 <Route path="/notes/:id" element={<Note note={note} />} />
@@ -161,11 +219,10 @@ const App = () => {
                 <Route path="/" element={<Home />} />
             </Routes>
 
-            <div>
-                <br />
+            <Footer>
                 <em>Note app, Department of Computer Science 2023</em>
-            </div>
-        </div>
+            </Footer>
+        </Page>
     )
 }
 
