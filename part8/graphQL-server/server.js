0089ed031ed8c501946f6babfd426c3cfd895f6d
talucre <talucreee@gmail.com>
2026-02-20 20:39:02 +0300

    server refactoring

---
+++ part8/graphQL-server/server.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
+const resolvers = require('./resolvers')
+const typeDefs = require('./schema')
+
+const startServer = port => {
+    const server = new ApolloServer({ typeDefs, resolvers })
+
+    startStandaloneServer(server, { listen: { port: 4000 } }).then(
+        ({ url }) => {
+            console.log(`Server ready at ${url}`)
+        },
+    )
+}
+
+module.exports = startServer
