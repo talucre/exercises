459763a3ea3c0fc94874ecf1a96f1c4ec26812bb
talucre <talucreee@gmail.com>
2026-02-21 21:03:20 +0300

    reafactoring resolvers to use database

--- part8/graphQL-server/resolvers.js
+++ part8/graphQL-server/resolvers.js
@@ -1,41 +1,17 @@
 const { GraphQLError } = require('graphql/error')
-const { v1: uuid } = require('uuid')
-
-let persons = [
-    {
-        name: 'Arto Hellas',
-        phone: '040-123543',
-        street: 'Tapiolankatu 5 A',
-        city: 'Espoo',
-        id: '3d594650-3436-11e9-bc57-8b80ba54c431',
-    },
-    {
-        name: 'Matti Luukkainen',
-        phone: '040-432342',
-        street: 'Malminkaari 10 A',
-        city: 'Helsinki',
-        id: '3d599470-3436-11e9-bc57-8b80ba54c431',
-    },
-    {
-        name: 'Venla Ruuska',
-        street: 'Nallemäentie 22 C',
-        city: 'Helsinki',
-        id: '3d599471-3436-11e9-bc57-8b80ba54c431',
-    },
-]
+const Person = require('./models/person')
 
 const resolvers = {
     Query: {
-        personCount: () => persons.length,
-        allPersons: (root, args) => {
+        personCount: async () => Person.collection.countDocuments(),
+        allPersons: async (root, args) => {
             if (!args.phone) {
-                return persons
+                return Person.find({})
             }
-            const byPhone = person =>
-                args.phone === 'YES' ? person.phone : !person.phone
-            return persons.filter(byPhone)
+
+            return Person.find({ phone: { $exists: args.phone === 'YES' } })
         },
-        findPerson: (root, args) => persons.find(p => p.name === args.name),
+        findPerson: async (root, args) => Person.findOne({ name: args.name }),
     },
     Person: {
         address: ({ street, city }) => {
@@ -43,8 +19,10 @@ const resolvers = {
         },
     },
     Mutation: {
-        addPerson: (root, args) => {
-            if (persons.find(p => p.name === args.name)) {
+        addPerson: async (root, args) => {
+            const nameExists = await Person.exists({ name: args.name })
+
+            if (nameExists) {
                 throw new GraphQLError(`Name must be unique: ${args.name}`, {
                     extensions: {
                         code: 'BAD_USER_INPUT',
@@ -53,21 +31,50 @@ const resolvers = {
                 })
             }
 
-            const person = { ...args, id: uuid() }
-            persons = persons.concat(person)
+            const person = new Person({ ...args })
+
+            try {
+                await person.save()
+            } catch (error) {
+                throw new GraphQLError(
+                    `Saving person failed ${error.message}`,
+                    {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.name,
+                            error,
+                        },
+                    },
+                )
+            }
+
             return person
         },
-        editNumber: (root, args) => {
-            const person = persons.find(p => p.name === args.name)
+        editNumber: async (root, args) => {
+            const person = await Person.findOne({ name: args.name })
+
             if (!person) {
                 return null
             }
 
-            const updatedPerson = { ...person, phone: args.phone }
-            persons = persons.map(p =>
-                p.name === args.name ? updatedPerson : p,
-            )
-            return updatedPerson
+            person.phone = args.phone
+
+            try {
+                await person.save()
+            } catch (error) {
+                throw new GraphQLError(
+                    `Saving number failed ${error.message}`,
+                    {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.name,
+                            error,
+                        },
+                    },
+                )
+            }
+
+            return person
         },
     },
 }
